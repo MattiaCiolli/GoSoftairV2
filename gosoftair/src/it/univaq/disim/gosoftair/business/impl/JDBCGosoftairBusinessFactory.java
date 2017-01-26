0b12b0a2d1ee51acd6dc150c59adeb000751aaea
Matteo Polsinelli <netrider91@gmail.com>
2017-01-26 17:01:45 +0100

    update user e password

--- gosoftair/src/it/univaq/disim/gosoftair/business/impl/JDBCGosoftairBusinessFactory.java
+++ gosoftair/src/it/univaq/disim/gosoftair/business/impl/JDBCGosoftairBusinessFactory.java
@@ -6,8 +6,8 @@ import it.univaq.disim.gosoftair.business.EventoService;
 public class JDBCGosoftairBusinessFactory extends GosoftairBusinessFactory {
 
 	private static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-	private static final String username ="sistemi";
-	private static final String password = "sistemi";
+	private static final String username ="gosoftair";
+	private static final String password = "gosoftair";
 		
 	@Override
 	public EventoService getEventoService()  {
