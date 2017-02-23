380c5eb876af3d5a58ef4234aa5becdc2a8b7988
Davide Giancola <gia.davide@gmail.com>
2017-02-23 11:33:03 +0100

    aumentato dimensione massima descrizione ann

--- GoSoftair/src/it/univaq/disim/gosoftair/presentation/CreaAnnuncioServlet.java
+++ GoSoftair/src/it/univaq/disim/gosoftair/presentation/CreaAnnuncioServlet.java
@@ -74,12 +74,10 @@ public class CreaAnnuncioServlet extends HttpServlet {
 			immagine = fileName;
 
 			String outputPath = savePath + File.separator + "annunci" + File.separator + "big";
-			System.out.println("write 1: "+outputPath + File.separator + "original" + fileName);
 			filePart.write(savePath + File.separator + "original" + fileName);
 			ImagesMap.generateImagesCard(outputPath, fileName, 362, 270, savePath, false);
 
 			outputPath = savePath + File.separator + "annunci" + File.separator + "small";
-			System.out.println("write 2: "+outputPath + File.separator + "original" + fileName);
 			ImagesMap.generateImagesCard(outputPath, fileName, 350, 200, savePath, true);
 			
 		}
